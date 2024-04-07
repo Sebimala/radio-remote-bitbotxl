8bb2276a542b5cb0a1b552e653c222b074eda5a1
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-07 14:57:19 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-acceleration = 0
+def update():
+    basic.clear_screen()
+    led.plot(ledX, ledY)
+ledYold = 0
+ledXold = 0
+dir2 = 0
+acc = 0
 ledY = 0
 ledX = 0
-led.set_brightness(0)
+led.set_brightness(128)
 basic.show_leds("""
     . . . . .
     . . . . .
@@ -11,9 +17,21 @@ basic.show_leds("""
     """)
 
 def on_forever():
-    global ledX, ledY, acceleration
+    global ledX, ledY, acc, dir2, ledXold, ledYold
     ledX = Math.round((input.acceleration(Dimension.X) + 1023) / 500)
     ledY = Math.round((input.acceleration(Dimension.Y) + 1023) / 500)
-    acceleration = Math.round(input.acceleration(Dimension.X) / 10)
-    direction = Math.round(input.acceleration(Dimension.Y) / 10)
+    acc = Math.round(input.acceleration(Dimension.X) / 10)
+    dir2 = Math.round(input.acceleration(Dimension.Y) / 10)
+    if ledX != ledXold:
+        ledXold = ledX
+        update()
+    if ledY != ledYold:
+        ledYold = ledY
+        update()
+    if acc != accold:
+        ledXold = ledX
+        update()
+    if ledY != ledYold:
+        ledYold = ledY
+        update()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
+enum RadioMessage {
+    message1 = 49434
+}
 function update () {
     basic.clearScreen()
     led.plot(ledX, ledY)
+    radio.sendValue("acc", acc)
+    radio.sendValue("dir", dir)
 }
+let dirold = 0
+let accold = 0
 let ledYold = 0
 let ledXold = 0
 let dir = 0
 let acc = 0
 let ledY = 0
 let ledX = 0
+radio.setGroup(69)
 led.setBrightness(128)
 basic.showLeds(`
     . . . . .
@@ -27,6 +35,14 @@ basic.forever(function () {
     }
     if (ledY != ledYold) {
         ledYold = ledY
+        update()
+    }
+    if (acc != accold) {
+        accold = acc
+        update()
+    }
+    if (dir != dirold) {
+        dirold = dir
         update()
     }
 })
