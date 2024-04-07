e746662c9566df22e2a0115158759872705fe0ff
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-07 15:41:26 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,28 @@
 enum RadioMessage {
     message1 = 49434
 }
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("acc", 100)
+    radio.sendValue("dir", 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (silencio == 0) {
+        silencio = 1
+    } else {
+        silencio = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("acc", 0)
+    radio.sendValue("dir", 0)
+})
 function update () {
     basic.clearScreen()
-    led.plot(ledX, ledY)
-    radio.sendValue("acc", acc)
-    radio.sendValue("dir", dir)
+    if (silencio == 0) {
+        led.plot(ledX, ledY)
+        radio.sendValue("acc", acc)
+        radio.sendValue("dir", dir)
+    }
 }
 let dirold = 0
 let accold = 0
@@ -15,8 +32,10 @@ let dir = 0
 let acc = 0
 let ledY = 0
 let ledX = 0
+let silencio = 0
 radio.setGroup(69)
 led.setBrightness(128)
+silencio = 0
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -27,8 +46,8 @@ basic.showLeds(`
 basic.forever(function () {
     ledX = Math.round((input.acceleration(Dimension.X) + 1023) / 500)
     ledY = Math.round((input.acceleration(Dimension.Y) + 1023) / 500)
-    acc = Math.round(input.acceleration(Dimension.X) / 10)
-    dir = Math.round(input.acceleration(Dimension.Y) / 10)
+    dir = Math.round(input.acceleration(Dimension.X) / 10)
+    acc = Math.round(input.acceleration(Dimension.Y) / 10)
     if (ledX != ledXold) {
         ledXold = ledX
         update()
